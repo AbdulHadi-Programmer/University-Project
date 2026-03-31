9a32f5116bfb598d34497634f823a19eb3a6545d
Abdul Hadi <122735121+AbdulHadi-Programmer@users.noreply.github.com>
2026-03-31 15:20:28 +0500

    Refactor URL imports and clean up urlpatterns

--- academy_tracker/accounts/urls.py
+++ academy_tracker/accounts/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path
-from .views import register_user, login_view, logout_view, profile, edit_profile, home , timetable, verify_email, resend_verification 
-
+from .views import (register_user, login_view, logout_view, profile, edit_profile, home, timetable)
 
 urlpatterns = [
-    path("", home, name="home"),  # homepage
+    path("", home, name="home"),
     path("register/", register_user, name="register"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("profile/", profile, name="profile"),
     path("profile/edit/", edit_profile, name="edit_profile"),
-    # path('timetable/', timetable, name="timetable")
-    path("register/", register_user, name="register"),
-    path("verify/<uuid:token>/", verify_email, name="verify_email"),
-    path("resend-verification/", resend_verification, name="resend_verification"),
-
+    path("timetable/", timetable, name="timetable"),
 ]
