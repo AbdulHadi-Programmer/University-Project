fb56a4f862ad41d62e870e728174e8c961cbf208
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2026-04-15 16:15:36 +0500

    some settings.py changes

--- academy_tracker/academy_tracker/settings.py
+++ academy_tracker/academy_tracker/settings.py
@@ -106,15 +106,17 @@ AUTHENTICATION_BACKENDS = [
 from decouple import config
 # EMAIL_BACKEND = config('EMAIL_BACKEND')
 # Use this with a default:
-EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
-EMAIL_HOST = config('EMAIL_HOST', default="smtp.gmail.com")
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+# EMAIL_HOST = config('EMAIL_HOST', default="smtp.gmail.com")
+# EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'studymate378@gmail.com'
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 from django.conf import settings
