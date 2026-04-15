6c41d465dff5d47d63703d24cb6725fab1138953
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2026-04-15 18:50:30 +0500

    broken everything

--- academy_tracker/academy_tracker/settings.py
+++ academy_tracker/academy_tracker/settings.py
@@ -111,13 +111,15 @@ from decouple import config
 # EMAIL_PORT = config('EMAIL_PORT', cast=int)
 # EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 # EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'studymate378@gmail.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = "byty ucjd lmpv ilnz bsog"
 DEFAULT_FROM_EMAIL = 'studymate378@gmail.com'
 
 from django.conf import settings
